2343d2181e5a56ecaf6437f5e921425a9ae39bfd
pedro-hackintosh <pcarota@hotmail.com>
2026-05-05 21:36:53 +0000

    Fix: Remove GJBaseGameLayer hook (function not in bindings)

--- loader/src/hooks/AntiStutter.cpp
+++ loader/src/hooks/AntiStutter.cpp
@@ -1,5 +1,4 @@
 #include <Geode/modify/CCDirector.hpp>
-#include <Geode/modify/GJBaseGameLayer.hpp>
 #include <Geode/loader/Log.hpp>
 #include "utils/AntiStutterManager.hpp"
 
@@ -9,15 +8,5 @@ struct AntiStutterCCDirectorHook : Modify<AntiStutterCCDirectorHook, cocos2d::CC
     void update(float dt) {
         AntiStutterManager::get().update(dt);
         CCDirector::update(dt);
-    }
-};
-
-struct AntiStutterGJBaseGameLayerHook : Modify<AntiStutterGJBaseGameLayerHook, GJBaseGameLayer> {
-    void handlePlayerDeath(bool force) {
-        if (AntiStutterManager::get().isInSafeWindow()) {
-            log::debug("AntiStutter: Blocking death during safe window");
-            return;
-        }
-        GJBaseGameLayer::handlePlayerDeath(force);
     }
 };
